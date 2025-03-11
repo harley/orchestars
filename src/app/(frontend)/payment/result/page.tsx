7bcd8102416f8ff567b950c921cec5aceb6b0327
Harley Trung <harley@coderpush.com>
2025-03-11 14:01:48 +0700

    reset page.tsx

--- src/app/(frontend)/payment/result/page.tsx
+++ src/app/(frontend)/payment/result/page.tsx
@@ -1,17 +1,17 @@
+
 import React from 'react';
 import PaymentResultPage from './ResultPage';
 import { getPayload } from 'payload'
 import config from '@/payload.config'
 import { notFound } from 'next/navigation'
 
-interface PageProps {
-    params: Promise<any>;
-    searchParams: { [key: string]: string | undefined };
-}
+type SearchParams = Promise<{ apptransid: string }>
 
-const PaymentResult = async (props: PageProps) => {
-    const apptransid = props.searchParams.apptransid
-    console.log('params', props.searchParams)
+
+const PaymentResult = async (props: { searchParams: SearchParams }) => {
+
+    const params = await props.searchParams;
+    console.log('params', params)
 
     const payloadConfig = await config
     const payload = await getPayload({ config: payloadConfig })
@@ -19,10 +19,11 @@ const PaymentResult = async (props: PageProps) => {
     const paymentInfo = await payload.find({
         collection: 'payments',
         limit: 1,
-        where: { appTransId: { equals: apptransid } },
+        where: { appTransId: { equals: params.apptransid } },
     }).then(res => res.docs?.[0])
 
     console.log('paymentInfo', paymentInfo)
+
 
     if (!paymentInfo) {
         return notFound()
@@ -32,6 +33,6 @@ const PaymentResult = async (props: PageProps) => {
             <PaymentResultPage paymentInfo={paymentInfo} />
         </div>
     );
-}
+};
 
 export default PaymentResult;
