dd422d1cde2fd39e9af0fcfcdc54ead6ba75c044
hungng14 <hungnguyendnipr101196@gmail.com>
2025-03-24 18:09:40 +0700

    fix: delete conflict migration code (#118)

--- src/migrations/20250324_081044_add_orders_to_tickets.ts
+++ src/migrations/20250324_081044_add_orders_to_tickets.ts
@@ -9,19 +9,14 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
    WHEN duplicate_object THEN null;
   END $$;
   
-  CREATE INDEX IF NOT EXISTS "users_email_idx" ON "users" USING btree ("email");
   CREATE INDEX IF NOT EXISTS "tickets_order_idx" ON "tickets" USING btree ("order_id");
-  ALTER TABLE "users" DROP COLUMN IF EXISTS "role";
-  DROP TYPE "public"."enum_users_role";`)
+  `)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE "public"."enum_users_role" AS ENUM('customer');
   ALTER TABLE "tickets" DROP CONSTRAINT "tickets_order_id_orders_id_fk";
   
-  DROP INDEX IF EXISTS "users_email_idx";
   DROP INDEX IF EXISTS "tickets_order_idx";
-  ALTER TABLE "users" ADD COLUMN "role" "enum_users_role" DEFAULT 'customer';
   ALTER TABLE "tickets" DROP COLUMN IF EXISTS "order_id";`)
 }
