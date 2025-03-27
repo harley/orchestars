67067581bb9c8477e12ff528b89a424c1d743529
Harley Trung <harley@coderpush.com>
2025-03-27 09:00:38 +0700

    fix: add event admin role

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -27,6 +27,7 @@ import * as migration_20250323_112122_create_admin from './20250323_112122_creat
 import * as migration_20250323_112852_cleanup_users from './20250323_112852_cleanup_users';
 import * as migration_20250324_081044_add_orders_to_tickets from './20250324_081044_add_orders_to_tickets';
 import * as migration_20250325_045037_update_task_slug from './20250325_045037_update_task_slug';
+import * as migration_20250327_015955_add_event_admin_role from './20250327_015955_add_event_admin_role';
 
 export const migrations = [
   {
@@ -172,6 +173,11 @@ export const migrations = [
   {
     up: migration_20250325_045037_update_task_slug.up,
     down: migration_20250325_045037_update_task_slug.down,
-    name: '20250325_045037_update_task_slug'
+    name: '20250325_045037_update_task_slug',
+  },
+  {
+    up: migration_20250327_015955_add_event_admin_role.up,
+    down: migration_20250327_015955_add_event_admin_role.down,
+    name: '20250327_015955_add_event_admin_role'
   },
 ];
