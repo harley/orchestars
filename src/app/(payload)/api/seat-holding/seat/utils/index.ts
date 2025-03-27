b8d28dea573b1dc36007e7eb8871546aed4a5886
hungng14 <hungnguyendnipr101196@gmail.com>
2025-03-27 16:59:57 +0700

    fix: enhance order validation

--- src/app/(payload)/api/seat-holding/seat/utils/index.ts
+++ src/app/(payload)/api/seat-holding/seat/utils/index.ts
@@ -1,4 +1,62 @@
+import { SeatHolding } from '@/payload-types'
+import { sql } from '@payloadcms/db-postgres'
 import { BasePayload, Where } from 'payload'
+
+export const getExistingSeatHolding = async ({
+  eventId,
+  eventScheduleId,
+  notEqualSeatHoldingCode,
+  inSeats,
+  payload,
+}: {
+  eventId: number
+  eventScheduleId: string
+  notEqualSeatHoldingCode?: string
+  inSeats?: string[]
+  payload: BasePayload
+}) => {
+  const currentTime = new Date().toISOString()
+  const conditions: Where = {
+    event: { equals: Number(eventId) },
+    eventScheduleId: { equals: eventScheduleId },
+    closedAt: { exists: false },
+    expire_time: { greater_than: currentTime },
+  }
+
+  if (notEqualSeatHoldingCode) {
+    conditions.code = { not_equals: notEqualSeatHoldingCode }
+  }
+
+  if (inSeats?.length) {
+    conditions.seatName = { in: inSeats }
+  }
+
+  const existingSeats = await payload.db.drizzle
+    .execute(
+      `
+    SELECT sh.id, sh.seat_name as "seatName", sh.code, sh.expire_time
+      FROM seat_holdings sh
+      WHERE 
+        sh.event_id = ${Number(eventId)}
+        AND sh.event_schedule_id = '${eventScheduleId}'
+        AND sh.closed_at IS NULL
+        AND sh.expire_time >= '${currentTime}'
+        ${notEqualSeatHoldingCode ? sql` AND sh.code != '${notEqualSeatHoldingCode}'` : ''}
+        ${
+          inSeats?.length
+            ? ` AND EXISTS (
+                    SELECT 1 FROM unnest(string_to_array(sh.seat_name, ',')) AS seat(value) 
+                    WHERE trim(value) =  ANY('{"${inSeats.join('","')}"}')
+                  )`
+            : ''
+        }   
+  `,
+    )
+    .then((res) => res.rows as unknown as SeatHolding[])
+    .catch(() => [])
+
+  return existingSeats
+}
 
 export const getSeatHoldings = async ({
   eventId,
@@ -30,7 +88,7 @@ export const getSeatHoldings = async ({
       select: {
         id: true,
         seatName: true,
-        seatHoldingCode: true,
+        // code: true,
       },
       limit: 1000,
     })
