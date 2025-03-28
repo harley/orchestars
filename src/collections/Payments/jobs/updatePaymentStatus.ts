dea46e8e96acf995655d4bc334b7625184942b4d
hungng14 <hungnguyendnipr101196@gmail.com>
2025-03-28 14:20:07 +0700

    feat: cron job for updating expired payments (#138)

---
+++ src/collections/Payments/jobs/updatePaymentStatus.ts
@@ -0,0 +1,70 @@
+import { BasePayload } from 'payload'
+import { subMinutes } from 'date-fns'
+import { PAYMENT_STATUS } from '../constants'
+import { ORDER_STATUS } from '@/collections/Orders/constants'
+import { TICKET_STATUS } from '@/collections/Tickets/constants'
+
+export const updatePaymentStatus = async ({ payload }: { payload: BasePayload }) => {
+  try {
+    const last30minutes = subMinutes(new Date(), 30).toISOString()
+    console.log(
+      'Running update payment status: processing payments that expired after 30 minutes will be updated to cancel',
+    )
+
+    await payload.db.drizzle.execute(`
+        DO $$ 
+        DECLARE 
+        error_occurred BOOLEAN := FALSE;
+        BEGIN
+        BEGIN
+            -- Create a temporary table to store expired order IDs, auto-drop on commit
+            CREATE TEMP TABLE temp_expired_orders ON COMMIT DROP AS
+            SELECT order_id 
+            FROM payments
+            WHERE status = '${PAYMENT_STATUS.processing.value}' 
+            AND expire_at < '${last30minutes}';
+
+
+            -- Update payments table
+            UPDATE payments
+            SET 
+            status = '${PAYMENT_STATUS.canceled.value}',
+            updated_at = NOW()
+            WHERE order_id IN (SELECT order_id FROM temp_expired_orders);
+
+            -- Update orders table
+            UPDATE orders
+            SET 
+            status = '${ORDER_STATUS.canceled.value}',
+            updated_at = NOW()
+            WHERE id IN (SELECT order_id FROM temp_expired_orders);
+
+            -- Update tickets table
+            UPDATE tickets
+            SET 
+            status = '${TICKET_STATUS.cancelled.value}',
+            updated_at = NOW()
+            WHERE order_id IN (SELECT order_id FROM temp_expired_orders);
+
+            -- Log success message
+            RAISE NOTICE 'Transaction completed successfully: payments, orders, and tickets updated.';
+
+        EXCEPTION
+            WHEN OTHERS THEN
+            error_occurred := TRUE;
+            RAISE NOTICE 'Transaction failed: %, rolling back changes.', SQLERRM;
+            ROLLBACK; -- Rollback the entire transaction
+        END;
+
+        -- If there was an error, raise an exception to notify
+        IF error_occurred THEN
+            RAISE EXCEPTION 'Rollback executed due to an error.';
+        END IF;
+
+        END $$;
+
+      `)
+  } catch (error) {
+    console.log('Error while updating payment status', error)
+  }
+}
