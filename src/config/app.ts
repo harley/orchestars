c86574aaf1ac2c2b428b6236b7e559791dd82362
hungng14 <hungnguyendnipr101196@gmail.com>
2025-04-02 10:39:51 +0700

    fix: set vi as default language (#142)

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,1 +1,3 @@
 export const APP_BASE_URL = process.env.APP_BASE_URL || 'http://localhost:3000'
+export const DEFAULT_FALLBACK_LOCALE =
+  (process.env.NEXT_PUBLIC_DEFAULT_FALLBACK_LOCALE as 'vi' | 'en') || 'vi'
--- src/payload-config/i18n/index.ts
+++ src/payload-config/i18n/index.ts
@@ -3,13 +3,14 @@ import { en } from '@payloadcms/translations/languages/en'
 import { vi } from '@payloadcms/translations/languages/vi'
 import extraEnLanguage from './locales/en.json'
 import extraViLanguage from './locales/vi.json'
+import { DEFAULT_FALLBACK_LOCALE } from '@/config/app'
 
 export const i18n: Config['i18n'] = {
   translations: {
     en: extraEnLanguage,
     vi: extraViLanguage,
   },
-  fallbackLanguage: 'en',
+  fallbackLanguage: DEFAULT_FALLBACK_LOCALE,
   supportedLanguages: {
     en: en as any,
     vi: vi as any,
