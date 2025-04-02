eefa520b2eb1bfac7cc504987e256ed48424a924
Ruby Bui <ruby@coderpush.com>
2025-04-02 03:23:23 -0500

    remove status

--- src/collections/CheckInRecords/index.ts
+++ src/collections/CheckInRecords/index.ts
@@ -11,7 +11,7 @@ export const CheckInRecords: CollectionConfig = {
   },
   admin: {
     useAsTitle: 'ticketCode',
-    defaultColumns: ['ticketCode', 'event', 'checkInStatus', 'checkInTime'],
+    defaultColumns: ['ticketCode', 'event', 'checkInTime'],
   },
   fields: [
     {
