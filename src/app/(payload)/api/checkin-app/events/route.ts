3aaa2cc103b1c1ea0327f1b69b3d634f897230f1
Ruby Bui <ruby@coderpush.com>
2025-04-14 22:59:32 -0500

    feat: add get published event for set event and schedule screen (#164)

---
+++ src/app/(payload)/api/checkin-app/events/route.ts
@@ -0,0 +1,44 @@
+// POST /api/checkin-app/events
+// use token to authorize user
+// get all events with event schedule 
+
+import { NextResponse } from 'next/server'
+import { getPayload } from 'payload'
+import config from '@/payload.config'
+import { headers as getHeaders } from 'next/headers'
+// import { isAdminOrSuperAdminOrEventAdmin } from '@/access/isAdminOrSuperAdmin'
+// import { getClientSideURL } from '@/utilities/getURL'
+
+export async function GET() {
+    // Get authorization header
+    const payload = await getPayload({ config })
+
+    const headers = await getHeaders()
+    const { user } = await payload.auth({ headers })
+
+    if (!user) {
+        return NextResponse.json(
+            { error: 'Unauthorized - Invalid admin user' },
+            { status: 401 },
+        )
+    }
+
+    // Get all events with event schedule
+    const events = await payload.find({
+        collection: 'events',
+        where: {
+            status: {
+                equals: 'published_open_sales',
+            },  
+    }})
+    console.log(events)
+    if (!events.docs?.length) {
+        return NextResponse.json({ error: 'No event is found' }, { status: 404 })
+    }
+   
+    console.log(events.docs)
+    return NextResponse.json({
+          events
+        }, { status: 200 })
+
+}
