4b5f2bb8c0e30f3fc41db997adf7639f9a9686c5
Viet Pham <pxviet1997@gmail.com>
2025-04-24 12:08:57 +0700

    add now showing

--- src/components/Home/Component.client.tsx
+++ src/components/Home/Component.client.tsx
@@ -10,6 +10,7 @@ import { Partner } from '@/types/Partner'
 import { Performer } from '@/types/Performer'
 import { Event } from '@/types/Event'
 import { Activity } from '@/payload-types'
+import ConcertList from './components/ConcertList'
 
 const PastConcerts = dynamic(() => import('./components/PastConcerts'))
 const PerformersSection = dynamic(() => import('./components/PerformersSection'))
@@ -18,6 +19,7 @@ const ActivitiesSection = dynamic(() => import('./components/Activities'))
 
 const HomeClient = ({
   bannerDocs,
+  onGoingPaginatedDocs,
   partners,
   performers,
   pastEvents,
@@ -37,6 +39,12 @@ const HomeClient = ({
 
   const visibleSections = useMemo(() => {
     const array = []
+    if (onGoingPaginatedDocs?.docs?.length > 0) {
+      array.push({
+        Component: ConcertList,
+        props: { onGoingPaginatedDocs },
+      })
+    }
     if (pastEvents?.length > 0) {
       array.push({ Component: PastConcerts, props: { events: pastEvents } })
     }
--- src/components/Home/components/ConcertList.tsx
+++ src/components/Home/components/ConcertList.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useRef } from 'react'
-import { Calendar, MapPin } from 'lucide-react'
-import { format as dateFnsFormat } from 'date-fns'
 
+import Image from 'next/image'
 import { PaginatedDocs } from 'payload'
 import { cn } from '@/utilities/ui'
 import Link from 'next/link'
 import { useTranslate } from '@/providers/I18n/client'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import { EVENT_STATUS } from '@/collections/Events/constants/status'
 interface ConcertListProps {
   onGoingPaginatedDocs: PaginatedDocs
   className?: string
@@ -40,92 +47,71 @@ const ConcertList: React.FC<ConcertListProps> = ({ onGoingPaginatedDocs, classNa
   }, [])
 
   return (
-    <section className={`py-20 ${className || ''}`}>
-      <div className="container mx-auto px-4">
-        <div className="text-center mb-12">
-          <h2 className="text-4xl font-extrabold bg-gradient-to-r from-gray-700 to-gray-950 bg-clip-text text-transparent">
-            {t('home.onGoingEvent')}
-          </h2>
-          <div className="w-24 h-1 bg-gradient-to-r from-gray-950 to-gray-700 mx-auto mt-4 rounded-full" />
-        </div>
-
-        <div className="space-y-20">
-          {onGoingPaginatedDocs?.docs?.map((evt, index) => (
-            <div
-              key={evt.id}
-              ref={(el) => (elementsRef.current[index] = el) as any}
-              className={cn(
-                'grid md:grid-cols-2 gap-6 md:gap-10 animate-on-scroll',
-                index % 2 === 0 ? 'md:grid-flow-col' : '',
-              )}
-              style={{ animationDelay: `${index * 0.1}s` }}
-            >
-              {/* Image section */}
-              <div
-                className={cn(
-                  'rounded-lg overflow-hidden h-[250px] sm:h-[300px] md:h-full shadow-md',
-                  index % 2 === 0 ? 'md:order-1' : 'md:order-2',
-                )}
-              >
-                <img
-                  src={evt.eventBanner?.url || evt.eventThumbnail?.url}
-                  alt={evt.eventBanner?.alt || evt.eventThumbnail?.alt || evt.title}
-                  className="w-full h-full object-cover transition-transform duration-700 hover:scale-105"
-                />
-              </div>
-
-              {/* Content section */}
-              <div
-                className={cn(
-                  'flex flex-col justify-center',
-                  index % 2 === 0 ? 'md:order-2' : 'md:order-1',
-                )}
-              >
-                <Link
-                  href={`/events/${evt.slug}`}
-                  className="text-2xl md:text-3xl font-bold mb-3 hover:underline"
-                >
-                  {evt.title}
-                </Link>
-                <div className="flex flex-col font-medium gap-1 text-muted-foreground mb-4">
-                  <div className="flex items-center">
-                    <Calendar className="h-4 w-4 mr-2" />
-                    <span className="text-sm">
-                      {dateFnsFormat(new Date(evt.startDatetime), 'dd/MM/yyyy HH:mm a')} -{' '}
-                      {dateFnsFormat(new Date(evt.endDatetime), 'dd/MM/yyyy HH:mm a')}
-                    </span>
-                  </div>
-                  {evt.eventLocation && (
-                    <div className="flex items-center">
-                      <MapPin className="h-4 w-4 mr-2" />
-                      <span className="text-sm">{evt.eventLocation}</span>
-                    </div>
-                  )}
-
-                  {/* <div className="flex items-center">
-                    <Users className="h-4 w-4 mr-2" />
-                    <span className="text-sm">{'-/300'} attendees</span>
-                  </div> */}
-                </div>
-
-                <pre className="text-muted-foreground mb-6 text-sm whitespace-pre-wrap font-gilroy">
-                  {evt.description}
-                </pre>
-
-                <div>
-                  <Link
-                    // variant="primary"
-                    href={`/events/${evt.slug}`}
-                    className="text-sm px-4 py-2 bg-gray-800 text-white hover:bg-gray-800/90 shadow-subtle relative cursor-pointer rounded-lg font-medium inline-flex items-center justify-center transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary/20"
-                  >
-                    {t('home.bookTicket')}
-                  </Link>
-                </div>
-              </div>
+    <section className={`py-10 md:py-20 ${className || ''}`}>
+      <Carousel className="w-full relative">
+        <div className="container mx-auto px-4">
+          <div className="flex md:items-center gap-2 justify-between mb-4">
+            <h2 className="text-2xl md:text-4xl font-bold uppercase">
+              {t('home.nowShowingEvents')}
+            </h2>
+            <div className="flex space-x-4">
+              <CarouselPrevious className="relative inset-0 translate-y-0 bg-black/30 hover:bg-black/80 hover:text-white text-white rounded-full h-10 w-10" />
+              <CarouselNext className="relative inset-0 translate-y-0 bg-black/30 hover:bg-black/80 hover:text-white text-white rounded-full h-10 w-10" />
             </div>
-          ))}
+          </div>
+
+          <CarouselContent className="-ml-4">
+            {onGoingPaginatedDocs?.docs
+              .filter((evt) => evt.status === EVENT_STATUS.published_open_sales.value)
+              .map((evt, index) => (
+                <CarouselItem key={evt.id} className="pl-4">
+                  <div
+                    key={evt.id}
+                    ref={(el) => (elementsRef.current[index] = el) as any}
+                    className={cn(
+                      'grid md:grid-cols-2 gap-6 md:gap-10 animate-on-scroll',
+                      index % 2 === 0 ? 'md:grid-flow-col' : '',
+                    )}
+                    style={{ animationDelay: `${index * 0.1}s` }}
+                  >
+                    {/* Image section */}
+                    <div className="relative rounded-lg overflow-hidden w-full h-[250px] sm:h-[600px] lg:h-[890px] shadow-md">
+                      <Image
+                        fill
+                        src={evt.eventThumbnail?.url || evt.eventBanner?.url}
+                        alt={evt.eventThumbnail?.alt || evt.eventBanner?.alt || evt.title}
+                        className="object-cover object-center transition-transform duration-700 hover:scale-105"
+                      />
+                    </div>
+
+                    {/* Content section */}
+                    <div className="flex flex-col justify-end">
+                      <Link
+                        href={`/events/${evt.slug}`}
+                        className="text-[22px] md:text-6xl font-bold mb-3 hover:underline"
+                      >
+                        {evt.title}
+                      </Link>
+                      <pre className="text-muted-foreground mb-6 text-lg md:text-xl whitespace-pre-wrap font-gilroy">
+                        {evt.description}
+                      </pre>
+
+                      <div>
+                        <Link
+                          // variant="primary"
+                          href={`/events/${evt.slug}`}
+                          className="text-sm px-4 py-2 bg-gray-800 text-white hover:bg-gray-800/90 shadow-subtle relative cursor-pointer rounded-lg font-medium inline-flex items-center justify-center transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary/20"
+                        >
+                          {t('home.bookTicket')}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+          </CarouselContent>
         </div>
-      </div>
+      </Carousel>
     </section>
   )
 }
