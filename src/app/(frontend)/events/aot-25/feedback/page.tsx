c3eee96664aeebedf9ee721179e1d407be6762f4
Viet Pham <pxviet1997@gmail.com>
2025-04-25 14:09:59 +0700

    add forms

--- src/app/(frontend)/events/aot-25/feedback/page.tsx
+++ src/app/(frontend)/events/aot-25/feedback/page.tsx
@@ -2,7 +2,7 @@ const EventDisney25 = () => {
   return (
     <div className="flex justify-center items-center h-screen pt-20">
       <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLScjW23mQKm9tXELNMlzDStnGuf1nbyM-9hmd5KxXYfatzs8tA/viewform?embedded=true"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfQSlFSzn39ZZtJ4Zv3BuCs8fd1ApZVGEc9Cqi-VNa6XPhdyg/viewform?embedded=true"
         width="100%"
         height="100%"
         frameBorder="0"
