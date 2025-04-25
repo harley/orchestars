87ffb8685533e1f06890bd25cb0f05f70b8d0b7d
Viet Pham <56182628+pxviet1997@users.noreply.github.com>
2025-04-25 14:32:27 +0700

    feat: Add Forms for Orchestars (#229)

---
+++ src/app/(frontend)/events/aot-25/feedback/page.tsx
@@ -0,0 +1,18 @@
+const EventAOT25Feedback = () => {
+  return (
+    <div className="flex justify-center items-center h-screen pt-20">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfQSlFSzn39ZZtJ4Zv3BuCs8fd1ApZVGEc9Cqi-VNa6XPhdyg/viewform?embedded=true"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Loading…
+      </iframe>
+    </div>
+  )
+}
+
+export default EventAOT25Feedback
