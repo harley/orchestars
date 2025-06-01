c83648f4b785478ef1bf6158d807ddf84137cd91
Harley Trung <harley@coderpush.com>
2025-06-01 09:28:25 +0700

    chore: styling

--- src/components/EventDetail/EventBanner.tsx
+++ src/components/EventDetail/EventBanner.tsx
@@ -105,7 +105,7 @@ const EventBanner = ({ event }: { event: Event }) => {
             {/* Right Content - Event Banner Image */}
             {event.slug === 'disney-25' ? (
               <div className="relative w-full md:min-w-[200px] md:max-w-[40vw] flex-shrink-0 flex-grow-0 flex items-center justify-center p-4 md:p-0">
-                <div className="relative w-full h-[60vw] max-h-[400px] md:h-[500px] aspect-[3/4] flex items-center justify-center">
+                <div className="relative w-full h-[60vw] md:h-[500px] aspect-[3/4] flex items-center justify-center">
                   <Image
                     src={(event.eventThumbnail as Media)?.url || '/images/logos/logo-black-adjacent.png'}
                     alt={event.title || 'Event'}
