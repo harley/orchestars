8fbc04df50e4d4243159fd07633441261c9aa675
hungng14 <hungnguyendnipr101196@gmail.com>
2025-06-20 09:03:24 +0700

    feat: implement affiliate ranking system

---
+++ src/collections/Affiliate/constants/index.ts
@@ -0,0 +1,17 @@
+export const AFFILIATE_RANK = {
+  seller: { label: 'Seller', value: 'seller' },
+  fan: { label: 'Fan', value: 'fan' },
+  ambassador: { label: 'Ambassador', value: 'ambassador' },
+  patron: { label: 'Patron', value: 'patron' },
+}
+
+export const AFFILIATE_RANKS = Object.values(AFFILIATE_RANK)
+
+
+export const AFFILIATE_RANK_STATUS = {
+  draft: { label: 'Draft', value: 'draft' },
+  active: { label: 'Active', value: 'active' },
+  disabled: { label: 'Disabled', value: 'disabled' },
+}
+
+export const AFFILIATE_RANK_STATUSES = Object.values(AFFILIATE_RANK_STATUS)
