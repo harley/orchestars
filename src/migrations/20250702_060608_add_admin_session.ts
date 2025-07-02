41eb4f47f5d13d27879ba8bd372c237dd22e9db2
hungng14 <hungnguyendnipr101196@gmail.com>
2025-07-02 13:53:17 +0700

    feat: add admin session table and related changes (#336)
    ## ✨ Summary by Git AI
    
    ### 🔥 Changes
    - Follow the update of the payloadcms version 3.44.0 to add fields
    needed for the database
    - Adds a new `admins_sessions` table to store admin session information.
    - Alters the `enum_payload_jobs_log_task_slug` type to include
    'createCollectionExport'.
    - Alters the default subject of `forms_emails_locales`.
    - Creates indexes and foreign key constraints for the `admins_sessions`
    table.
    - Includes migration functions for both up and down operations.
                  
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
      * Introduced a new session management system for administrators.
      * Enhanced support for job tasks by adding a new export option.
    * **Improvements**
    * Updated the default subject line for form notification emails to a
    more user-friendly message.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ src/migrations/20250702_060608_add_admin_session.ts
@@ -0,0 +1,34 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TYPE "public"."enum_payload_jobs_log_task_slug" ADD VALUE 'createCollectionExport' BEFORE 'schedulePublish';
+  ALTER TYPE "public"."enum_payload_jobs_task_slug" ADD VALUE 'createCollectionExport' BEFORE 'schedulePublish';
+  CREATE TABLE "admins_sessions" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"created_at" timestamp(3) with time zone,
+  	"expires_at" timestamp(3) with time zone NOT NULL
+  );
+  
+  ALTER TABLE "forms_emails_locales" ALTER COLUMN "subject" SET DEFAULT 'You''ve received a new message.';
+  ALTER TABLE "admins_sessions" ADD CONSTRAINT "admins_sessions_parent_id_fk" FOREIGN KEY ("_parent_id") REFERENCES "public"."admins"("id") ON DELETE cascade ON UPDATE no action;
+  CREATE INDEX "admins_sessions_order_idx" ON "admins_sessions" USING btree ("_order");
+  CREATE INDEX "admins_sessions_parent_id_idx" ON "admins_sessions" USING btree ("_parent_id");`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "admins_sessions" DISABLE ROW LEVEL SECURITY;
+  DROP TABLE "admins_sessions" CASCADE;
+  ALTER TABLE "payload_jobs_log" ALTER COLUMN "task_slug" SET DATA TYPE text;
+  DROP TYPE "public"."enum_payload_jobs_log_task_slug";
+  CREATE TYPE "public"."enum_payload_jobs_log_task_slug" AS ENUM('inline', 'schedulePublish');
+  ALTER TABLE "payload_jobs_log" ALTER COLUMN "task_slug" SET DATA TYPE "public"."enum_payload_jobs_log_task_slug" USING "task_slug"::"public"."enum_payload_jobs_log_task_slug";
+  ALTER TABLE "payload_jobs" ALTER COLUMN "task_slug" SET DATA TYPE text;
+  DROP TYPE "public"."enum_payload_jobs_task_slug";
+  CREATE TYPE "public"."enum_payload_jobs_task_slug" AS ENUM('inline', 'schedulePublish');
+  ALTER TABLE "payload_jobs" ALTER COLUMN "task_slug" SET DATA TYPE "public"."enum_payload_jobs_task_slug" USING "task_slug"::"public"."enum_payload_jobs_task_slug";
+  ALTER TABLE "forms_emails_locales" ALTER COLUMN "subject" SET DEFAULT 'You''''ve received a new message.';`)
+}
