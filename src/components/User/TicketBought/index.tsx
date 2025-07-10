2cf6a744d68420282a04928bc3737c0508fe03ae
Mia Pham <pham222h@mtholyoke.edu>
2025-07-10 15:11:37 +0700

    feat: Add Ticket Type filtering in user profile. Refactored duplicate Ticket components"

--- src/components/User/TicketBought/index.tsx
+++ src/components/User/TicketBought/index.tsx
@@ -2,10 +2,11 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslate } from '@/providers/I18n/client'
 // import { TimeFilterTabs } from '@/components/User/TicketBought/TimeFilterTabs'
-import UpcomingTickets from './UpcomingTickets'
-import FinishedTickets from './FinishedTickets'
+import TicketsList from './TicketsList'
+import { useTickets } from '@/components/User/hooks/useTickets'
+import { TicketTypeFilterTab } from './TicketTypeFilterTab'
 
-const TicketBought = ({className}: {className?: string}) => {
+const TicketBought = ({ className }: { className?: string }) => {
   const { t } = useTranslate()
   const [mounted, setMounted] = useState(false)
 
@@ -15,17 +16,44 @@ const TicketBought = ({className}: {className?: string}) => {
 
   const containerClass = `max-w-4xl mx-auto bg-white rounded-2xl shadow-xl p-8 mt-8 transition-all duration-700 ${mounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'} ${className || ''}`
 
-  const [timeFilter, _setTimeFilter] = useState<'upcoming' | 'finished'>('upcoming')
+  const [timeFilter, setTimeFilter] = useState<'upcoming' | 'finished'>('finished')
+
+  const [ticketType, setTicketType] = useState<'booked' | 'cancelled' | 'gifted'>('booked')
+
+  const {
+    tickets,
+    isLoading,
+    error,
+    currentPage,
+    totalPages,
+    hasNextPage,
+    hasPrevPage,
+    nextPage,
+    prevPage,
+  } = useTickets({ eventType: timeFilter, ticketType: ticketType })
 
   return (
     <div className={containerClass}>
       <h1 className="text-2xl font-bold my-4">{t('userprofile.title')}</h1>
 
       {/* <TimeFilterTabs timeFilter={timeFilter} setTimeFilter={setTimeFilter} t={t} /> */}
+      <TicketTypeFilterTab
+        typeFilter={ticketType}
+        setTypeFilter={setTicketType}
+        t={t}
+      ></TicketTypeFilterTab>
 
-      <UpcomingTickets className={timeFilter === 'upcoming' ? '' : 'hidden'} />
-
-      <FinishedTickets className={timeFilter === 'finished' ? '' : 'hidden'} />
+      <TicketsList
+        tickets={tickets}
+        isLoading={isLoading}
+        error={error}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        hasNextPage={hasNextPage}
+        hasPrevPage={hasPrevPage}
+        nextPage={nextPage}
+        prevPage={prevPage}
+      />
     </div>
   )
 }
