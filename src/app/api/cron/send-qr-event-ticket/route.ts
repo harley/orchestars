b021467262ac8d364da11f32c807e2d1a398d38e
hungng14 <hungnguyendnipr101196@gmail.com>
2025-07-18 15:12:39 +0700

    feat: send qr event ticket within 30 hours before event start

--- src/app/api/cron/send-qr-event-ticket/route.ts
+++ src/app/api/cron/send-qr-event-ticket/route.ts
@@ -28,12 +28,20 @@ export async function GET(req: NextRequest) {
     // Initialize Payload
     const payload = await getPayload()
 
-    // get current event open for sales
+    // get the event only that will show before 30 hours to event start time
+    const now = new Date()
+    const thirtyHoursFromNow = new Date(now.getTime() + 30 * 60 * 60 * 1000)
+
     const event = await payload
       .find({
         collection: 'events',
         where: {
-          endDatetime: { greater_than_equal: new Date() },
+          startDatetime: {
+            less_than_equal: thirtyHoursFromNow.toISOString(),
+          },
+          endDatetime: {
+            greater_than_equal: now.toISOString(),
+          },
           status: {
             in: [EVENT_STATUS.published_upcoming.value, EVENT_STATUS.published_open_sales.value],
           },
@@ -48,23 +56,32 @@ export async function GET(req: NextRequest) {
     }
 
     const queryCompletedOrders = await payload.db.drizzle.execute(sql`
-      SELECT DISTINCT ON (ord.id)
-      ord.id,
-      ord.user_id,
-      ord.order_code,
-      tk.event_id AS event_id,
-      es.date AS event_date,
-      usr.email AS user_email
-    FROM orders ord
-    LEFT JOIN users usr ON ord.user_id = usr.id
-    LEFT JOIN tickets tk ON ord.id = tk.order_id AND tk.status = ${TICKET_STATUS.booked.value}
-    LEFT JOIN events_schedules es ON tk.event_schedule_id = es.id
-    LEFT JOIN emails em ON ord.id = em.order_id AND em.type = ${EMAIL_TYPE.qr_event_ticket.value}
-    WHERE ord.status = ${ORDER_STATUS.completed.value}
-      AND tk.event_id = ${event.id}
-      AND em.id IS NULL
-    ORDER BY ord.id, es.date ASC
-    LIMIT ${batchSize}
+      SELECT * FROM (
+        SELECT DISTINCT ON (ord.id)
+          ord.id,
+          ord.user_id,
+          ord.order_code,
+          tk.event_id AS event_id,
+          e.start_datetime AS event_start_time,
+          e.end_datetime AS event_end_time,          
+          es.date AS event_date,
+          (es.date::date + (e.start_datetime::time at time zone 'UTC')) at time zone 'UTC' as full_current_event_date_time,
+          usr.email AS user_email
+        FROM orders ord
+        LEFT JOIN users usr ON ord.user_id = usr.id
+        LEFT JOIN tickets tk ON ord.id = tk.order_id AND tk.status = ${TICKET_STATUS.booked.value}
+        LEFT JOIN events_schedules es ON tk.event_schedule_id = es.id
+        LEFT JOIN events e ON tk.event_id = e.id
+        LEFT JOIN emails em ON ord.id = em.order_id AND em.type = ${EMAIL_TYPE.qr_event_ticket.value}
+        WHERE ord.status = ${ORDER_STATUS.completed.value}
+          AND tk.event_id = ${event.id}
+          AND em.id IS NULL
+          ORDER BY ord.id, es.date ASC
+      ) AS completed_orders
+      WHERE completed_orders.full_current_event_date_time <= ${thirtyHoursFromNow.toISOString()} 
+      AND completed_orders.full_current_event_date_time >= ${now.toISOString()}
+      ORDER BY completed_orders.event_date ASC
+      LIMIT ${batchSize}
     `)
 
     const orders = (queryCompletedOrders as { rows: any[] }).rows.map((row) => ({
@@ -73,7 +90,10 @@ export async function GET(req: NextRequest) {
       orderCode: row.order_code,
       eventId: row.event_id,
       eventDate: row.event_date,
+      fullCurrentEventDateTime: row.full_current_event_date_time,
       userEmail: row.user_email,
+      eventStartTime: row.event_start_time,
+      eventEndTime: row.event_end_time,
     }))
 
     // Format event date/time information
