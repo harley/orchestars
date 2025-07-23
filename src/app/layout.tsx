840c7f98b9736c58e1d8a6e94a62b16fe682bce3
hungng14 <hungnguyendnipr101196@gmail.com>
2025-07-23 22:33:15 +0700

    refactor: integrate next/third-parties/google and streamline GA tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-// import { GA_ID } from '@/lib/gtag'
+import { GA_ID } from '@/config/tracking'
 import { GoogleAnalytics } from '@next/third-parties/google'
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID || ''
   return (
     <html lang="en" suppressHydrationWarning>
       <head></head>
       <body>
         {children}
-        <GoogleAnalytics gaId={GA_ID} />
+        {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
       </body>
     </html>
   )
--- src/lib/gtag.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-  interface Window {
-    gtag: (...args: any[]) => void
-  }
-}
-export {}
